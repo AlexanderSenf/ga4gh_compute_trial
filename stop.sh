65bcd699b9ec69e916f4f298e52960aa8a4603de
Alexander Senf <asenf@enthought.com>
2020-02-18 11:30:32 +0000

    Fix stop script.

--- stop.sh
+++ stop.sh
@@ -13,8 +13,8 @@ docker stop tes_tes-funnel_1 || true && docker rm tes_tes-funnel_1 || true
 #killall -9 funnel
 killall -9 wes
 docker stop pro_wes_flower_1 || true && docker rm pro_wes_flower_1 || true
-docker stop pro_wes_protes-worker_1 || true && docker rm pro_wes_protes-worker_1 || true
+docker stop pro_wes_prowes-worker_1 || true && docker rm pro_wes_prowes-worker_1 || true
 docker stop pro_wes_rabbitmq_1 || true && docker rm pro_wes_rabbitmq_1 || true
-docker stop pro_wes_protes_1 || true && docker rm pro_wes_protes_1 || true
+docker stop pro_wes_prowes_1 || true && docker rm pro_wes_prowes_1 || true
 docker stop pro_wes_mongodb_1 || true && docker rm pro_wes_mongodb_1 || true
 
